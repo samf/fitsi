2db4779b9e4da7a9bdc39f8e107164351dab805a
Sam Falkner <sam.falkner@gmail.com>
2026-05-27 06:29:47 -0600

    make samf/samf/fitsi a homebrew formula
    with some automation to go with it

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "fitsi",
+    targets: [
+        .executableTarget(
+            name: "fitsi",
+            path: "fitsi"
+        )
+    ]
+)
